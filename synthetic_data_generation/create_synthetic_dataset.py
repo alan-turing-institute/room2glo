80827641a113f51a2cb1f3d37b5b896f46c9a6e8
pjshoemark <pjshoemark@gmail.com>
2019-11-15 14:54:43 +0000

    additional comments

--- synthetic_data_generation/create_synthetic_dataset.py
+++ synthetic_data_generation/create_synthetic_dataset.py
@@ -27,18 +27,39 @@ class TweetYielder(object):
 
 
 def maybe_return_pseudoword(context_word, year_month_index):
+	"""
+	Decide whether to replace an instance of a context_word with its corresponding pseudoword.
+	"""
 
+	# look up which pseudoword the current context_word is associated with.
 	pseudoword = context_words[context_word]['pseudoword']
+	
+	# look up which schema the associated pseudoword belongs to.
 	pseudoword_type = context_words[context_word]['pseudoword_type']
+	
+	# look up which 'set' the context_word belongs to (i.e. what 'kind' of context_word it is)
+	# e.g. Schema C2 pseudowords have one or more context_words which gradually decrease in frequency over
+	# time (set 1), and one or more context_words which gradually increase in frequency over time (set 2).
 	set_number = context_words[context_word]['set_number']
 
-	if pseudoword_type  == 'D4' or (pseudoword_type  == 'C3' and set_number == 1):
+	# Schema C3 pseudowords and Schema D4 pseudowords each have a set of context_words whose replacement probabilities at 
+	# a given time-step are specified by a multinomial distibution drawn with a Dirichlet prior. 
+	if pseudoword_type  == 'D4' or (pseudoword_type  == 'C3' and set_number == 1):	
+		
+		# So, if the current context_word is associated with a C3 or D4 pseudoword, we need to look up which index in 
+		# the multinomial distribution corresponds to it.
 		dist_index = context_words[context_word]['dist_index']
+		
+		# We then retrieve the pseudoword-insertion-probability, given the current context_word's set number, the 
+		# current time-step, and the current context_word's index in the current time-step's multinomial distribution.
 		p_insert = pseudoword_insert_probs[pseudoword]["p{}_array_series".format(set_number)][year_month_index][dist_index]
 
 	else:
 		try:
+			# retrieve the pseudoword-insertion-probability given the current context_word's set number, and the 
+			# current time-step.
 			p_insert = pseudoword_insert_probs[pseudoword]["p{}_series".format(set_number)][year_month_index]
+		
 		except TypeError:
 			print(pseudoword)
 			print(context_word)
@@ -46,36 +67,61 @@ def maybe_return_pseudoword(context_word, year_month_index):
 			raise()
 
 
+			
+			
+	# set 'insert' to 1 with probability p_insert, or to 0 with probability 1-p_insert
 	insert = np.random.RandomState().choice(2,1,p=[1-p_insert, p_insert])
 
+	# if 'insert' was set to 1, we will replace the current context_word with its associated pseudoword
 	if insert:
 		return pseudoword
+	
+	# if 'insert' was set to 0, we will not replace the current context_word.
 	else:
 		return False
 
 
 
 def create_synthetic(year_month_index, subsampling, subsample_percent):
+	"""
+	Create the synthetic data for a given timestep (year_month_index is simply an integer in the range (0, n_timesteps) 
+	specifying which timestep we are at). 
+	
+	If subsampling is not set to 0, then lines will be randomly subsampled, keeping only
+	subsample_percent of them.
+	
+	For each line that is kept, we identify instances of the words that were selected to represent
+	senses of pseudowords, and with the probabilities specified in our pseudoword design dictionaries,
+	we replace them with their corresponding pseudowords.
+	"""
+	
 
 	year_month = year_months[year_month_index]
 
+	# create the output file for the current time-step, and open it for writing
 	with gzip.open(os.path.join(outfiles_rootdir, year_month[:4], year_month), 'wt') as outfile:
 
+			# load the input data (which is the same for every time-step)
 			tweets = TweetYielder([input_filepath])
 
-			for tweet in tweets:
+			for tweet in tweets: #i.e. for each line of the input file..
 
+				# if subsampling, discard the current line with probability 1 - subsample_percent
 				if subsampling:
 					if np.random.RandomState().random_sample() > subsample_percent*0.01:
 						continue
 
+				# if we didn't discard the current line, we will now go through one token at a time and
+				# decide whether to replace each token with a pseudoword.
 				tweet_out = []
-
-		
 				for i in range(len(tweet)):
 
-
+					# if the current token is one of the words we have chosen to represent a pseudoword
+					# 'sense'...
 					if tweet[i] in context_words:
+						
+						# ...then we replace it with its associated pseudoword, with a probability 
+						# specified by our pseudoword design dictionaries.
 						pseudoword = maybe_return_pseudoword(tweet[i], year_month_index)
 						if pseudoword:
 							tweet_out.append(pseudoword)
@@ -83,7 +129,8 @@ def create_synthetic(year_month_index, subsampling, subsample_percent):
   							tweet_out.append(tweet[i])
 					else:
 						tweet_out.append(tweet[i])
-
+						
+				# finally, we write this potentially-modified line to our synthetic data file.
 				outfile.write(' '.join(tweet_out)+'\n')
 	return year_month
 
@@ -184,7 +231,7 @@ if __name__ == "__main__":
 	
 
 
-	# load the dictionaries.
+	# load the dictionaries created that were previously created using design_pseudowords.py.
 	with open(context_word_dict_filepath, 'r') as infile:
 		context_words = json.load(infile)
 
@@ -192,14 +239,16 @@ if __name__ == "__main__":
 		pseudoword_insert_probs = json.load(infile)
 
 
+		
 
 	pool = multiprocessing.Pool()  #defaults to the number of Cores on the machine
 
-	for year_month_index in range(n_timesteps):
+	for year_month_index in range(n_timesteps): # i.e. for each timestep...
 
+		# to run single-threaded, uncomment the following line and comment out the next 6.
 		# create_synthetic(year_month_index, subsampling, subsampling_percent)
+		
 		pool.apply_async(create_synthetic, (year_month_index, subsampling, subsampling_percent), callback=success, error_callback=error)
-
 	try:
 		pool.close() 
 		pool.join() 
